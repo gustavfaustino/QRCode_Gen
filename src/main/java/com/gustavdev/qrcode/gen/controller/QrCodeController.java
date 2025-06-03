bef89b112a6a99d4105961b6f00ccd7649f83256
gustavfaustino <gustav.faustino107@gmail.com>
2025-06-03 18:46:34 -0300

    Implement QR code generation logic in QrCodeController

--- src/main/java/com/gustavdev/qrcode/gen/controller/QrCodeController.java
+++ src/main/java/com/gustavdev/qrcode/gen/controller/QrCodeController.java
@@ -8,13 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavdev.qrcode.gen.dtos.qrcode.QrCodeGenerateResponse;
 import com.gustavdev.qrcode.gen.dtos.qrcode.QrCodeGeneratorRequest;
+import com.gustavdev.qrcode.gen.services.QrCodeGeneratorService;
 
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGeneratorRequest request){
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.getText());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new QrCodeGenerateResponse("Error generating QR Code: " + e.getMessage()));
+        }
     }
 }
