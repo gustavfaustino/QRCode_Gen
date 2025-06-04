1c258c0e130aad894d144e858e4f0494d9551cbc
gustavfaustino <gustav.faustino107@gmail.com>
2025-06-03 23:59:49 -0300

    Refactor error handling in QrCodeController to catch specific exceptions for improved clarity

--- src/main/java/com/gustavdev/qrcode/gen/controller/QrCodeController.java
+++ src/main/java/com/gustavdev/qrcode/gen/controller/QrCodeController.java
@@ -1,11 +1,14 @@
 package com.gustavdev.qrcode.gen.controller;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.zxing.WriterException;
 import com.gustavdev.qrcode.gen.dtos.qrcode.QrCodeGenerateResponse;
 import com.gustavdev.qrcode.gen.dtos.qrcode.QrCodeGeneratorRequest;
 import com.gustavdev.qrcode.gen.services.QrCodeGeneratorService;
@@ -25,8 +28,7 @@ public class QrCodeController {
         try {
             QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
             return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (WriterException | IOException e) {
             return ResponseEntity.status(500).body(new QrCodeGenerateResponse("Error generating QR Code: " + e.getMessage()));
         }
     }
